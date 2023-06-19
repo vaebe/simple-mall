9cf1ecb669c0f9f1cf3868bb8cdafa6e850ee71a
zcj <18137693952@163.com>
2023-06-19 09:09:09 +0800

    style(主题): 在 app.vue 执行一次 useDark 页面初始化时判断主题自动设置

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 import 'dayjs/locale/zh-cn';
+import { useDark } from '@vueuse/core';
 
 // 设置系统名称
 const { VITE_APP_TITLE } = import.meta.env;
 document.title = VITE_APP_TITLE;
+
+useDark();
 </script>
 
 <template>
