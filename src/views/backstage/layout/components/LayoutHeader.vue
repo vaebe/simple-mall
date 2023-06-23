2dbe7ec25f8e329bc1c592e85f3ad089e59ca9a4
zcj <18137693952@163.com>
2023-06-23 22:07:56 +0800

    feat(后台管理): header 优化，增加logo，用户头像、昵称展示

--- src/views/backstage/layout/components/LayoutHeader.vue
+++ src/views/backstage/layout/components/LayoutHeader.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-import { CircleClose } from '@element-plus/icons-vue';
 import { useUserStore } from '@/store';
-import { defineAsyncComponent } from 'vue';
+import { defineAsyncComponent, reactive } from 'vue';
+import { useRouter } from 'vue-router';
 
 const ThemeSwitch = defineAsyncComponent(
   () => import('@/components/ThemeSwitch.vue')
@@ -9,21 +9,45 @@ const ThemeSwitch = defineAsyncComponent(
 
 const { VITE_APP_TITLE } = import.meta.env;
 
-const { loginOut } = useUserStore();
+const { loginOut, getUserInfo } = useUserStore();
+const userInfo = reactive(getUserInfo());
+
+const router = useRouter();
+const jumpHome = () => {
+  router.push('/');
+};
 </script>
 
 <template>
   <div class="header-box">
-    <div class="ml-4 font-medium text-2xl">
-      <p>{{ VITE_APP_TITLE }}</p>
+    <div class="ml-4 flex items-center">
+      <el-image class="h-8 mr-2" src="/public/vite.svg" fit="fill"></el-image>
+      <p class="cursor-pointer font-medium text-2xl" @click="jumpHome">
+        {{ VITE_APP_TITLE }}
+      </p>
     </div>
 
     <div class="flex items-center justify-end pr-4">
       <theme-switch></theme-switch>
 
-      <el-icon color="red" :size="20" class="ml-4" @click="loginOut">
-        <circle-close />
-      </el-icon>
+      <div class="user-info">
+        <el-popover placement="bottom" :width="100" trigger="hover">
+          <ul class="text-center">
+            <li class="mb-2 truncate">{{ userInfo.nickName }}</li>
+            <li class="cursor-pointer hover:text-blue-400" @click="loginOut">
+              退出登录
+            </li>
+          </ul>
+
+          <template #reference>
+            <el-image
+              class="h-8 w-8 rounded-full ml-2"
+              :src="userInfo.avatar"
+              fit="fill"
+            ></el-image>
+          </template>
+        </el-popover>
+      </div>
     </div>
   </div>
 </template>
