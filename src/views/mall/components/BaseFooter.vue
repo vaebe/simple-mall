11aaa85031ff9ea92cd4c1ebdf35d5955c30a233
zcj <18137693952@163.com>
2023-06-26 22:32:38 +0800

    feat(购物前台): 增加 BaseFooter 组件

---
+++ src/views/mall/components/BaseFooter.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="pb-4 text-center text-sm text-gray-400">
+    <p>Released under the MIT License.</p>
+    <p>Copyright © 2023-present vaebe.</p>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
