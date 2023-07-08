9a85b12a588a917d4bedc74c577a3b0f7bddb406
zcj <18137693952@163.com>
2023-07-08 15:52:10 +0800

    feat(商城前台): 个人中心 layout 布局

---
+++ src/views/mall/personalCenter/layout/index.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { useUserStore } from '@/store';
+import { storeToRefs } from 'pinia';
+
+const userStore = useUserStore();
+const { userInfo } = storeToRefs(userStore);
+
+const route = useRoute();
+
+const routerList = computed(() => route.matched[2].children);
+</script>
+
+<template>
+  <div class="mt-4 w-full xl:w-10/12 grid grid-cols-6 gap-4">
+    <el-card shadow="never" class="min-h-[83vh]">
+      <el-avatar :src="userInfo.avatar" />
+      <p class="truncate">{{ userInfo.nickName }}</p>
+
+      <el-divider></el-divider>
+
+      <ul>
+        <li
+          v-for="item in routerList"
+          :key="item.name"
+          :class="[
+            'cursor-pointer',
+            'hover:text-blue-400',
+            { 'text-blue-500': item.name === route.name }
+          ]"
+        >
+          {{ item.meta?.title }}
+        </li>
+      </ul>
+    </el-card>
+
+    <el-card shadow="never" class="min-h-[83vh] col-span-5">
+      <router-view></router-view>
+    </el-card>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
