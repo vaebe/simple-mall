ef0a973c4518daf629d69a3bd67357f21d51c27c
zcj <18137693952@163.com>
2023-07-15 19:50:06 +0800

    feat: 增加备案号

--- src/views/mall/layout/components/BaseFooter.vue
+++ src/views/mall/layout/components/BaseFooter.vue
@@ -4,6 +4,14 @@
   <div class="pb-4 text-center text-sm text-gray-400">
     <p>Released under the MIT License.</p>
     <p>Copyright © 2023-present vaebe.</p>
+    <p>
+      <el-link
+        href="https://beian.miit.gov.cn/#/Integrated/index"
+        target="_blank"
+      >
+        豫ICP备2022013746号
+      </el-link>
+    </p>
   </div>
 </template>
 
