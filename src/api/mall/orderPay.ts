7b239feae1d6efb57e46f1cff0e24e52a70dc7c2
zcj <18137693952@163.com>
2023-07-19 22:23:50 +0800

    feat(商品前台): 订单支付页面

---
+++ src/api/mall/orderPay.ts
@@ -0,0 +1,18 @@
+import Api from '../base';
+import type { ResultData } from '../base';
+
+export interface WeChatPayParams {
+  info: string;
+  orderId: string;
+  price: number;
+}
+
+export interface weChatPayRes {
+  code_url: string;
+  QRcode_url: string;
+}
+
+// 微信支付
+export const weChatPay = (
+  data: WeChatPayParams
+): Promise<ResultData<weChatPayRes>> => Api.post('/pay/weChatPay', data);
--- src/views/mall/orderPay/index.vue
+++ src/views/mall/orderPay/index.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+import { reactive, ref } from 'vue';
+import { getOrderDetails } from '@/api/backstage/dataManagement/order';
+import type { OrderDetails } from '@/api/backstage/dataManagement/order';
+import { concatenateAddressParts, getCodeNameByCodeId } from '@/utils/tool.ts';
+import { weChatPay } from '@/api/mall/orderPay';
+import { useEnums } from '@/composables';
+
 const props = defineProps({
   orderId: {
     type: String,
@@ -6,12 +13,111 @@ const props = defineProps({
   }
 });
 
+const { orderStatusEnums, getOrderStatusEnums } = useEnums();
+getOrderStatusEnums();
+
+const QRCodeLoading = ref(false);
+const QRCodeUrl = ref('');
+
+// 获取支付二维码
+const getPaymentQRCode = () => {
+  QRCodeLoading.value = true;
+  const opts = {
+    info: details.products.map((item) => item.name).join(','),
+    orderId: 'mall' + details.id,
+    price: details.totalPrice
+  };
+  weChatPay(opts)
+    .then((res) => {
+      QRCodeUrl.value = res.data.QRcode_url;
+    })
+    .finally(() => {
+      QRCodeLoading.value = false;
+    });
+};
+
+const details = reactive({
+  id: 0,
+  createdAt: '',
+  updatedAt: '',
+  userId: 0,
+  state: '',
+  totalPrice: 0,
+  remark: '',
+  addressId: 0,
+  paymentMethod: '',
+  shippingMethod: '',
+  products: [],
+  addressInfo: {}
+}) as OrderDetails;
+
+const getDetails = () => {
+  if (!props.orderId) {
+    return;
+  }
+
+  getOrderDetails({ id: parseInt(props.orderId) }).then((res) => {
+    Object.assign(details, res.data);
+
+    getPaymentQRCode();
+  });
+};
+getDetails();
+
 console.log(props.orderId);
 </script>
 
 <template>
-  <div class="text-2xl h-[100vh]">
-    此处应为订单支付页面，但是我还没有写！！！
+  <div class="h-[100vh]">
+    <!--   不是待支付状态 -->
+    <div v-if="details.state !== '00'">
+      <p class="text-4xl">
+        订单处于
+        {{ getCodeNameByCodeId(details.state, orderStatusEnums) }}
+        状态，无法进行支付！
+      </p>
+
+      <div class="flex justify-center mt-4">
+        <el-button @click="$router.push('/mall')">点击返回首页</el-button>
+      </div>
+    </div>
+
+    <div v-else>
+      <h1 class="font-medium text-lg my-2">订单支付</h1>
+
+      <p class="text-2xl">订单提交成功！确认下方信息无误后进行付款！</p>
+
+      <p class="mt-8 text-red-300">请在30分钟内完成支付, 超时后将取消订单!</p>
+
+      <p class="mt-4">订单编号：{{ details.id }}</p>
+
+      <p class="mt-4">订单金额：{{ details.totalPrice }}元</p>
+
+      <p class="mt-4">
+        收货信息：{{ details.addressInfo.name }} {{ details.addressInfo.phone }}
+        {{ concatenateAddressParts(details.addressInfo) }}
+        {{ details.addressInfo.detailedAddress }}
+      </p>
+
+      <div class="flex mt-4">
+        <p>商品名称：</p>
+        <ul>
+          <li
+            v-for="item in details.products"
+            :key="item.productId"
+            class="mb-2"
+          >
+            {{ item.name }}
+          </li>
+        </ul>
+      </div>
+
+      <p class="font-medium text-lg my-2">请使用微信扫码进行支付</p>
+      <div v-loading="QRCodeLoading" style="width: 300px; height: 300px">
+        <el-image v-if="QRCodeUrl" :src="QRCodeUrl"></el-image>
+      </div>
+      <p class="text-sm mt-2">测试阶段，金额后端会强制设置为 0.1 放心支付！</p>
+    </div>
   </div>
 </template>
 
